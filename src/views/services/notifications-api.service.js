106b58a8d8fb879df85340f7c7b693417f3ea315
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2022-09-28 15:46:48 -0500

    feature/Notifications: add Notifications section

--- src/views/services/notifications-api.service.js
+++ src/views/services/notifications-api.service.js
@@ -33,3 +33,38 @@ export class NotificationsApiService {
         return http.get(`/notifications?${title}=${condicion}`);
     }
 }
+import http from '/core/services/http-common.js';
+
+export class NotificationsApiService {
+    getAll() {
+        return http.get('/notifications');
+    }
+
+    getById(id) {
+        return http.get(`/notifications/${id}`);
+    }
+
+    create(data) {
+        return http.post('/notifications', data);
+    }
+
+    update(id, data) {
+        return http.put(`/notifications/${id}`, data);
+    }
+
+    patch(id, data){
+        return http.patch(`/notifications/${id}`, data);
+    }
+
+    delete(id) {
+        return http.delete(`/notifications/${id}`);
+    }
+
+    findByTitle(title) {
+        return http.get(`/notifications?title=${title}`);
+    }
+
+    findBy(title,condicion) {
+        return http.get(`/notifications?${title}=${condicion}`);
+    }
+}
